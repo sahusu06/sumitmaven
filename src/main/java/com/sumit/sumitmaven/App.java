c57d9e46d7f94e1b0899f0a9af187d8c0340bd1c
Sumit <sumitsscet@gmail.com>
2022-09-12 18:36:34 +0530

    Signed-off-by: Sumit <sumitsscet@gmail.com>

--- src/main/java/com/sumit/sumitmaven/App.java
+++ src/main/java/com/sumit/sumitmaven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Sahu World!" );
+        System.out.println( "Sahu World Hero!" );
     }
     
 }
